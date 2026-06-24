b9687a41f81e4d612f9a95e6077d1a3eb2abb109
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-06-24 11:59:03 +0530

    proper indentation

--- week1_assignments/calculate_grade.py
+++ week1_assignments/calculate_grade.py
@@ -1,11 +1,11 @@
 
 
 
-num1 = int(input("Enter the marks of 1 subject  : "))
-num2 = int(input("Enter the marks of 2 subject  : "))
-num3 = int(input("Enter the marks of 3 subject  : "))
-num4 = int(input("Enter the marks of 4 subject  : "))
-num5 = int(input("Enter the marks of 5 subject  : "))
+num1 = int(input("Enter the marks of 1 sub : "))
+num2 = int(input("Enter the marks of 2 sub : "))
+num3 = int(input("Enter the marks of 3 sub : "))
+num4 = int(input("Enter the marks of 4 sub : "))
+num5 = int(input("Enter the marks of 5 sub : "))
 
 avg = round((num1 + num2 + num3 + num4 + num5) / 5, 2)  #logical part to calculate avg
 
